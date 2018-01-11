5b7f903d74b491454ee769691169a2d6801a7331
kripsy <kripsy93@yandex.ru>
2018-01-11 21:59:25 +0300

    add sormauth app

--- sormauth/views.py
+++ sormauth/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def sormauth(request):
+    return render(request, 'sormauth/sormauth.html', locals())
+
+
+
+
+def mainpage(request):
+    return render(request, 'sormauth/mainpage.html', locals())
