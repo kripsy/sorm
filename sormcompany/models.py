80081d48e116d01c04017058a55395b01a2a216c
kripsy <kripsy93@yandex.ru>
2018-03-06 00:33:41 +0300

    add sormauth app

--- sormcompany/models.py
+++ sormcompany/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class TypeCompany(models.Model):
-    name = models.CharField(max_length=90, unique= True, verbose_name= "Тип организационной единицы")
+    name = models.CharField(max_length=90, verbose_name= "Тип организационной единицы")
     id = models.AutoField(primary_key=True)
 
 class Company(models.Model):
-    name = models.CharField(max_length=90, unique= True, verbose_name= "Наименование организационной единицы")
-    type_company = models.OneToOneField('TypeCompany', verbose_name= "Тип организационной единицы")
+    name = models.CharField(max_length=90, verbose_name= "Наименование организационной единицы")
+    type_company = models.ForeignKey('TypeCompany', verbose_name= "Тип организационной единицы")
     parent_company = models.ForeignKey('Company', null= True, verbose_name= "Родительская организация")
     id = models.AutoField(primary_key=True)
